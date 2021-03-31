186abe10df286de0f3f2f4091b1c8129421b5732
Vic Graf <victoria.b.graf@gmail.com>
2021-03-31 11:17:32 +0200

    Update About.vue

--- airbean-assignment/src/views/About.vue
+++ airbean-assignment/src/views/About.vue
@@ -1,15 +1,39 @@
 <template>
-  <div>
-      <p>About</p>
+  <div class="wrapper">
+    <img src="../assets/graphics/graphics-header.svg" alt="">
+    <h1>Vårt kaffe</h1>
+    <p>Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio, grounds dripper, crema, strong whipped, variety extra iced id lungo half and half mazagran. Pumpkin spice.</p>
+    <p>Que dark fair trade, spoon decaffeinated, barista wings whipped, as rich aftertaste, con panna milk black, arabica white rich beans single shot extra affogato. So affogato macchiato sit extraction instant grinder seasonal organic, turkish single shot, single origin, and robusta strong to go so dripper. Viennese froth, grounds caramelization skinny aromatic cup kopi-luwak, fair trade flavour, frappuccino medium, café au lait flavour cultivar ut bar instant kopi-luwak.</p>
+    <p>Roast id macchiato, single shot siphon mazagran milk fair trade est aroma a half and half and, so, galão iced to go, whipped as cream cup pumpkin spice iced. At extra, rich grinder, brewed to go, steamed half and half at, that, percolator macchiato trifecta and body as arabica dripper. In galão black java milk sit trifecta, robusta, acerbic café au lait instant shop latte. Seasonal bar shop filter aroma id, crema, affogato viennese cultivar aftertaste, seasonal, percolator cream black, galão flavour, milk aromatic turkish skinny crema.</p>
+    <img src="../assets/graphics/eva-cortado.jpg" alt="">
+    <h3>Eva Cortado</h3>
+    <p>VD och Grundare</p>
+    <img class="footer-img" src="../assets/graphics/graphics-footer.svg" alt="">
   </div>
 </template>
 
 <script>
 export default {
     name: 'About'
+    // data() {
+    //   return {
+    //     infoTexts: [
+    //       { text: 'Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio, grounds dripper, crema, strong whipped, variety extra iced id lungo half and half mazagran. Pumpkin spice.'},
+    //       { text: 'Que dark fair trade, spoon decaffeinated, barista wings whipped, as rich aftertaste, con panna milk black, arabica white rich beans single shot extra affogato. So affogato macchiato sit extraction instant grinder seasonal organic, turkish single shot, single origin, and robusta strong to go so dripper. Viennese froth, grounds caramelization skinny aromatic cup kopi-luwak, fair trade flavour, frappuccino medium, café au lait flavour cultivar ut bar instant kopi-luwak.Roast id macchiato, single shot siphon mazagran milk fair trade est aroma a half and half and, so, galão iced to go, whipped as cream cup pumpkin spice iced. At extra, rich grinder, brewed to go, steamed half and half at, that, percolator macchiato trifecta and body as arabica dripper. In galão black java milk sit trifecta, robusta, acerbic café au lait instant shop latte. Seasonal bar shop filter aroma id, crema, affogato viennese cultivar aftertaste, seasonal, percolator cream black, galão flavour, milk aromatic turkish skinny crema.'}
+    //     ]
+    //   }
+    // }
 }
 </script>
 
-<style scoped>
+<style>
+.wrapper {
+  width:375px;
+  background:#F3E4E1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  position: relative;
+}
 
 </style>
